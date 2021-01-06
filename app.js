0cdca495b13855f580c299fa8b550ac7ccffb661
Jack Humphries <retrojetpacks@gmail.com>
2021-01-06 21:14:06 +0000

    Added db to list app

--- app.js
+++ app.js
@@ -1,69 +1,196 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js"); //My custom date module
+const _ = require("lodash");
 
 const app = express();
+let today = date.getDay();
 
-//Random info
-//Var cannot have block scoope, ie a var defined in an if or for {} is global..
-//use let, which does have block scope. Will be destructed once {} close
-
-//It is possible to push items into a const js array....
-const items = ["Buy food", "Cook food","Eat food"];
-const workItems = [];
-
-//Use EJS as app view engine
-//assumes folder called views exists
+//Use EJS as app view engine, assumes folder called views exists
 app.set('view engine', 'ejs');
 
 // Use body-parser, parses info from html (or .ejs) page to access form results
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //Site is not static, Express only serves up main access point (app.js) and views -->
 app.use(express.static("public"));
 
 
 
+//============ Setup Mongo database using mongoose ==============//
+mongoose.connect("mongodb://localhost:27017/todoListDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+//Define Items Database
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+//Mongoose models are capitalised, singular
+const Item = mongoose.model("Item", itemsSchema);
+
+const tea = new Item({
+  name: "Make tea"
+});
+
+const eat = new Item({
+  name: "Eat breakfast"
+});
+
+const shower = new Item({
+  name: "Take a shower"
+});
+
+const defaultItems = [tea, eat, shower];
+
+
+
+//Define Lists database
+const listSchema = {
+  listName: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+
+
 
 //============== Home page =============//
-app.get("/", function(req, res){
-  let today = date.getDate();
+app.get("/", function(req, res) {
 
-  //EJS step, passing key to .ejs
-  res.render("list", {listTitle: today, newItems: items});
-
+  //Find items in mongoDB
+  Item.find({}, function(err, foundItems) {
+    //If collection is empty
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfuly added default items to DB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      //EJS step, passing key to .ejs
+      res.render("list", {
+        listTitle: today,
+        listItems: foundItems
+      });
+    }
+  });
 });
 
-app.post("/", function(req, res) {
-  let item = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+//Site posts new item to server
+app.post("/", function(req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  if (listName === today){
+    console.log("Save to main list");
+    item.save(); //mongo saves to db
     //Redirects to home route to update newListItem
     res.redirect("/");
-  }
+  } else {
+    console.log("Need to use extra list");
+    console.log(listName);
+
+    List.findOne({listName: listName}, function(err, foundList){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Found list: " + foundList + ", now pushing: " + item);
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/" + listName);
+      }
+    });
+  };
+
+
 });
 
+//Site requests to delete item from server
+app.post("/delete", function(req, res) {
+
+  const checkedItemID = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === today) {
+    Item.deleteOne({_id: checkedItemID}, function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Deleted checked item");
+      }
+    });
+    res.redirect("/");
+  } else {
+    List.findOneAndUpdate({listName}, {$pull: {items: {_id: checkedItemID}}}, function(err, foundList){
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/" + listName);
+      }
+    })
+  }
 
 
-//============== Work page =============//
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newItems: workItems});
-})
+});
+
 
 
 
 //============== About page =============//
-app.get("/about", function(req,res){
-  res.render("about");
+app.get("/:customPage", function(req, res) {
+  //Modify with lodash
+  const customListName = _.capitalize(req.params.customPage);
+  console.log("custom name: " + customListName);
+
+  List.findOne({listName: customListName}, function(err, foundList){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundList){
+        //Show existing list
+        console.log("Found list: " + customListName);
+        res.render("list", {
+          listTitle: customListName,
+          listItems: foundList.items
+        });
+      } else {
+        //Create new list
+        console.log("Creating new list call: " + customListName);
+        const list = new List({
+          listName: customListName,
+          items: defaultItems
+        });
+        list.save(function(err, doc){
+          res.redirect("/" + customListName);
+        });
+      }
+    }
+  })
+
+
+
+
 })
 
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server up on 3000");
 });
